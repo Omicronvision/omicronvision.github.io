0af01cfc6aaef0c45bada868cfa22303a5c00ece
Omicronvision <45631533+Omicronvision@users.noreply.github.com>
2021-01-28 14:17:51 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -2,6 +2,7 @@ window.desktopcheck = function()
 {
     if(window.innerWidth>768)
     {
+        alert("MOBILE");
         document.getElementsById("firstBanner").style.content = "Mobile";
     }
 }
