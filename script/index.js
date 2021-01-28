e9c1554bcdab7c8c170734045b6841cadc302b3c
Omicronvision <45631533+Omicronvision@users.noreply.github.com>
2021-01-28 14:22:54 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-window.desktopcheck = function()
+function windowCheckDesktop()
 {
     if(window.innerWidth>768)
     {
@@ -10,4 +10,4 @@ window.desktopcheck = function()
     }
 }
 
-window.desktopcheck();
+windowCheckDesktop();
