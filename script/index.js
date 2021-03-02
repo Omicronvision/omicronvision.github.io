8fc6467d797a038083e07ded0f1f43e783a9e40d
Omicronvision <45631533+Omicronvision@users.noreply.github.com>
2021-03-02 18:55:41 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -3,7 +3,7 @@ function windowCheckDesktop()
     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     if(isMobile)
     {
-        alert("MOBILE");
+        alert("MOBILE 2");
         document.getElementById("bigTitle").style.color = "blue";
     } else
     {
