02eb41a7382a6dd6c7a587ba77d60c77c34efda3
Omicronvision <45631533+Omicronvision@users.noreply.github.com>
2021-03-05 19:21:51 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,13 +1,23 @@
-function windowCheckDesktop()
+window.addEventListener("load", function()
 {
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if(isMobile)
-    {
-        alert("MOBILE 3");
-    } else
-    {
-        //alert("ORDINATEUR");
-    }
-}
-
-windowCheckDesktop();
+	function windowCheckDesktop()
+	{
+    	var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    	if(isMobile)
+    	{
+    	    //alert("MOBILE");
+    	    let element1 = document.querySelector("#menu ul");
+    	    element1.style.display = "grid";
+    	    element1.style.gridTemplateColumns = "300px";
+    	    element1.style.gridAutoRows = "125px";
+    	    element1.style.alignContent = "center";
+    	    let element2 = document.querySelector("#menu ul li a");
+    	    element2.style.gridTemplateColumns = "100%";
+    	    element2.style.alignItems = "center";
+    	} else
+    	{
+    	    //alert("ORDINATEUR");
+    	}
+	}
+	windowCheckDesktop();
+});
